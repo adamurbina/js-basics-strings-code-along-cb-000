932f55b2de393f80b7bfc70a5a853c5dd4ec0683
Adam Urbina <adamurbina@gmail.com>
2019-01-11 22:48:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const currentUser = "Me";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting =  `Welcome, ${currentUser.slice(0,1)}`;
